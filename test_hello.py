21597d71d3c0d80961872028ba5122d1b5a36397
Hausdorff94 <jhonyal58@gmail.com>
2022-05-04 19:17:45 -0500

    Update format printing

--- test_hello.py
+++ test_hello.py
@@ -2,11 +2,11 @@ from hello import add, toyou, substract
 
 
 def setup_function(function):
-	print("Running Setup: %s" % {function.__name__})
+	print(f"Running Setup: {function.__name__}")
 	function.x = 10
 
 def teardown_function(function):
-	print("Running Teardown: %s" % {function.__name__})
+	print(f"Running Teardown: {function.__name__}")
 	del function.x
 
 
