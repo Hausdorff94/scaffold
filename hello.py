ffd82b6ea6b336c4279d248b40a024cd8148401b
Hausdorff94 <jhonyal58@gmail.com>
2022-05-04 19:01:30 -0500

    Update scripts and tests

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
-def add(x, y):
-    return x + y
+def toyou(x):
+    return "hi %s" % x
+
+def add(x):
+    return x + 1
+
+def substract(x):
+    return x - 1
 
 
-result = add(1, 2)
-print(f"This is the sum: 1 + 2 = {result}")
+
+
+#result = add(1, 2)
+#print(f"This is the sum: 1 + 2 = {result}")
